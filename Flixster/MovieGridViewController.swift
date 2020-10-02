da69c5a3b3446f06fdb46a3c2f5d10db7c4edeab
FloCiaglia <floriana.ciaglia@gmail.com>
2020-10-02 16:35:31 -0600

    Changed the size of posters

--- Flixster/MovieGridViewController.swift
+++ Flixster/MovieGridViewController.swift
@@ -23,8 +23,8 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         
-        layout.minimumLineSpacing = 4
-        layout.minimumInteritemSpacing = 4
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
         
         let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
         layout.itemSize = CGSize(width: width, height: width * 3/2)
